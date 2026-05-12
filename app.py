042e4f16a03651a72d4d3d6a15fc4eccbc467b7a
surya12343-dotcom <surya12343@gmail.com>
2026-05-12 13:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,36 @@
 import streamlit as st
 import pandas as pd
 
-FILE_NAME = "26-27 Student Id.xlsx"
-
-st.set_page_config(page_title="Admission Number Search", layout="centered")
+st.set_page_config(page_title="Admission Number Search")
 
 st.title("SRI SHENBAGA VINAYAGAR MAT HR SEC SCHOOL")
 st.subheader("Student Admission Number Search")
 
-excel_data = pd.read_excel(FILE_NAME, sheet_name=None)
+df = pd.read_csv("students.csv")
 
-all_students = pd.DataFrame()
+df.columns = [str(col).strip() for col in df.columns]
 
-for sheet_name, df in excel_data.items():
-    df.columns = [str(col).strip() for col in df.columns]
+name_col = None
+id_col = None
 
-    name_col = None
-    id_col = None
-
-    for col in df.columns:
-        if "name" in col.lower():
-            name_col = col
-        if "id" in col.lower() or "admission" in col.lower():
-            id_col = col
-
-    if name_col and id_col:
-        temp = df[[name_col, id_col]].copy()
-        temp.columns = ["Student Name", "Admission No"]
-        temp["Class"] = sheet_name
-        all_students = pd.concat([all_students, temp], ignore_index=True)
-
-all_students = all_students.dropna(subset=["Student Name"])
+for col in df.columns:
+    if "name" in col.lower():
+        name_col = col
+    if "id" in col.lower() or "admission" in col.lower():
+        id_col = col
 
 student_name = st.text_input("Enter Student Name")
 
 if st.button("Search"):
-    if student_name.strip() == "":
-        st.warning("Please enter student name")
+
+    result = df[
+        df[name_col]
+        .astype(str)
+        .str.contains(student_name, case=False, na=False)
+    ]
+
+    if result.empty:
+        st.error("No student found")
     else:
-        result = all_students[
-            all_students["Student Name"]
-            .astype(str)
-            .str.contains(student_name, case=False, na=False)
-        ]
-
-        if result.empty:
-            st.error("No student found. Please check spelling.")
-        else:
-            st.success("Student Found")
-            st.dataframe(result, hide_index=True)
-
+        st.dataframe(result[[name_col, id_col]], hide_index=True)
 
